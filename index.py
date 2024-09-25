7c0c409b7f733d3ef468e102e4758140186be849
Idungvictor <idungvictor@gmail.com>
2024-09-25 12:34:21 +0100

    third commit

--- index.py
+++ index.py
@@ -3,3 +3,10 @@ print("I am who i am")
 print("I am who i am")
 
 print("Jesus is Lord")
+
+booklist=["pharmacology","physiology","anatomy"]
+print(booklist)
+print(type(booklist))
+
+booklist.append("biology")
+print(booklist)
